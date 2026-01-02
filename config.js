f98c4ecf9f313b4c8b640c9484dd8dac98425769
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2026-01-01 23:49:32 -0800

    Update config.js

--- config.js
+++ config.js
@@ -26,5 +26,4 @@ module.exports = {
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
     READ_CMD: process.env.READ_CMD || "false",
     AUTO_VOICE: process.env.AUTO_VOICE || "false",
-    ANTI_BADWORD: process.env.ANTI_BADWORD || "false",
 };
